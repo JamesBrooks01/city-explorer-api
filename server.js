3ab914e74c655506ee578e15cf31cc528b6af59c
James Brooks <brooksjamesk01@gmail.com>
2022-03-25 18:48:49 -0700

    mimplemented starter code into serverr.js

--- server.js
+++ server.js
@@ -1,49 +1,68 @@
 'use strict';
 
-//REQUIRE
-// IN SERVERS, YOU USE 'REQUIRE' IN PLACE OF IMPORT
+// REQUIRE
 const express = require('express');
 require('dotenv').config();
-// WE MUST INCLUDE CORS IF WE WANT TO SHARE RESOURCES OVER THE WEB
 const cors = require('cors');
-const getMovies = require('./modules/movies');
-const getWeather = require('./modules/weather');
-const getLocation = require('./modules/location');
+const movie = require('./modules/movies');
+const weather = require('./modules/weather');
+const location = require('./modules/location');
 
 //USE
-// ONCE SOMETHING IS REQUIRED WE USE IT. THIS IS WEHERE WE ASSIGN THE REQUIRED FIELD A VARIABLE. REACT DOES IT IN ONE STEP WITH IMPORT AND EXPRESS DOES IT IN TWO WITH 'REQUIRE' AND 'USE'
 const app = express();
 app.use(cors());
-
-//DEFINE PORT AND VALIDATE .env FILE IS WORKING
 const PORT = process.env.PORT || 3002;
 // IF SERVER IS RUNNING ON 3002, SOMETHING IS WRONG WITH .env FILE OR HOW THE VALUES ARE BEING IMPORTED FROM IT.
 
 
 //ROUTES
-// HERE WE WRITE OUR ENDPOINTS
-// app.get() correlates to axios.get
-app.get('/', (requestObject, responseObject) => {
-  responseObject.send('breaking through to the other side!');
+app.get('/', (request, response) => {
+  response.send('The Code Works');
 });
 
-app.get('/location', getLocation);
-app.get('/weather', getWeather);
-app.get('/movies', getMovies);
+app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
+app.get('/movies', moviesHandler);
 
+//Handlers
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  weather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Sorry. Something went wrong!');
+    });
+}
 
-// THE LAST ROUTE IS A CATCH ALL OR STAR ROUTE
-app.get('*', (requestObject, responseObject) => {
-  responseObject.send('The page you are looking for doesn\'t exist');
+function moviesHandler(request, response) {
+  const keyword = request.query.keyword;
+  movie(keyword)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Sorry. Something went wrong!');
+    });
+}
+
+function locationHandler(request, response) {
+  const cityName = request.query.cityName;
+  location(cityName)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Sorry. Something went wrong!');
+    });
+}
+
+app.get('*', (request, response) => {
+  response.send('The page you are looking for doesn\'t exist');
 });
 
 //ERROR HANDLING
-
-//CLASS CONSTRUCTOR
-
-
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
 
 //LISTEN
-// START THE SERVER
-// LISTEN IS AN EXPRESS METHOD THAT TAKES IN A A PORT VALUE AND A CALLBACK FUNCTION
 app.listen(PORT, () => console.log(`Listening on Port: ${PORT}`));
